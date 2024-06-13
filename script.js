077b79aff4edde1e419e2624dc261f00c07dbdbf
unknown <ericjackwong@outlook.com>
2024-06-13 16:38:43 -0400

    Adds index file

---
+++ script.js
@@ -0,0 +1,117 @@
+// Get a reference to the #add-employees-btn element
+const addEmployeesBtn = document.querySelector('#add-employees-btn');
+//this is the button to start the process 
+
+//need a randomizer
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+  }
+
+// Collect employee data
+const collectEmployees = function() {
+  // TODO: Get user input to create and return an array of employee objects
+  //add the text box to get the data
+  let adding = true;
+  const employeeArray = []
+  while(adding){
+    //make 1 employee
+    //this wants me to make an employee object, and give it traits
+    const employee = {
+      firstName : prompt("Please enter first name"),
+      lastName : prompt("Please enter last name"),
+      salary : prompt("Please enter salary")
+    }
+    employee.salary = parseInt(employee.salary); //recast the salary as a number
+    employeeArray.push(employee); //add to employee list
+    adding = confirm("Add another employee?"); //add more employess
+  }
+  return employeeArray; //it is expected to return an array
+}
+
+// Display the average salary
+const displayAverageSalary = function(employeesArray) {
+  // TODO: Calculate and display the average salary
+  let total = 0;//get a running total
+  for (item of employeesArray){
+    total = total + item.salary; //iterate through and grab the salary
+  }
+  //it just wants the average salary in console log
+  //no line break, it will screw with the log
+  console.log(`The average employee salary between our ${employeesArray.length} employee(2) is $${total/employeesArray.length}`); 
+}
+
+
+// Select a random employee
+const getRandomEmployee = function(employeesArray) {
+  // TODO: Select and display a random employee
+  const winner = getRandomInt(employeesArray.length); //gets a random index within bounds
+  console.log(`Congratulations to ${employeesArray[winner].firstName} ${employeesArray[winner].lastName}, our random drawing winner!`);
+}
+
+/*
+  ====================
+  STARTER CODE
+  Do not modify any of the code below this line:
+*/
+
+// Display employee data in an HTML table
+const displayEmployees = function(employeesArray) {
+  // Get the employee table
+  const employeeTable = document.querySelector('#employee-table');
+
+  // Clear the employee table
+  employeeTable.innerHTML = '';
+
+  // Loop through the employee data and create a row for each employee
+  for (let i = 0; i < employeesArray.length; i++) {
+    const currentEmployee = employeesArray[i];
+
+    const newTableRow = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = currentEmployee.firstName;
+    newTableRow.append(firstNameCell);
+
+    const lastNameCell = document.createElement("td");
+    lastNameCell.textContent = currentEmployee.lastName;
+    newTableRow.append(lastNameCell);
+
+    const salaryCell = document.createElement("td");
+    // Format the salary as currency
+    //currentEmployee.salary requires an object with traits
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
+      style:"currency",
+      currency:"USD"
+    });
+
+    newTableRow.append(salaryCell);
+
+    employeeTable.append(newTableRow);
+  }
+}
+
+const trackEmployeeData = function() {
+  //returns many employees
+  const employees = collectEmployees();
+
+  console.table(employees);
+
+  displayAverageSalary(employees);
+
+  console.log('==============================');
+
+  getRandomEmployee(employees);
+
+  //sorts the employees by last name
+  employees.sort(function(a,b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  displayEmployees(employees);
+}
+
+// Add event listener to 'Add Employees' button
+addEmployeesBtn.addEventListener('click', trackEmployeeData);
